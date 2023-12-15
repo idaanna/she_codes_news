d6381fb932cd30c23edb7452c0d7b5af9b6ec606
Ida Eriksson <annaida.eriksson@gmail.com>
2023-12-15 19:04:16 +0800

    My profile view

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
 
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, MyProfileView 
 
 app_name = 'users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('my-profile/', MyProfileView.as_view(),name='myProfile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -4,8 +4,22 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.views.generic.base import TemplateView 
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class MyProfileView(TemplateView):
+    model = CustomUser
+    success_url = reverse_lazy('myProfile')
+    template_name = 'users/profileView.html'
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['author_stories'] = NewsStory.objects.filter(author=self.request.user)
+    #     return context
